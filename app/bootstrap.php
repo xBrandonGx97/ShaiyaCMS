a74bba0fe7a01ebc436f39aeb6e4b280d546e7fb
lunethdev <lunethdev@gmail.com>
2020-03-23 22:41:39 -0500

    removed dispatchFastRoute method, moved over to dispatch.

--- app/bootstrap.php
+++ app/bootstrap.php
@@ -77,8 +77,33 @@ class Bootstrap
 
     public function dispatch()
     {
-        // Load Routes
-        $this->dispatchFastRoute();
+        require_once ROUTES_PATH . 'routes.php';
+        // Fetch method and URI from somewhere
+        $httpMethod = $_SERVER['REQUEST_METHOD'];
+        $uri = $_SERVER['REQUEST_URI'];
+
+        // Strip query string (?foo=bar) and decode URI
+        if (false !== $pos = strpos($uri, '?')) {
+            $uri = substr($uri, 0, $pos);
+        }
+        $uri = rawurldecode($uri);
+
+        $routeInfo = $dispatcher->dispatch($httpMethod, $uri);
+
+        switch ($routeInfo[0]) {
+            case \FastRoute\Dispatcher::NOT_FOUND:
+                return abort(404);
+                break;
+            case \FastRoute\Dispatcher::METHOD_NOT_ALLOWED:
+                $allowedMethods = $routeInfo[1];
+                return abort(405);
+                break;
+            case \FastRoute\Dispatcher::FOUND:
+                $handler = $routeInfo[1];
+                $vars = $routeInfo[2];
+                return $routeInfo[1]($httpMethod);
+                break;
+        }
     }
 
     public function _is_ajax()
@@ -161,36 +186,5 @@ class Bootstrap
         $rootDir = dirname(dirname(__FILE__));
         $dotenv = Dotenv::createImmutable($rootDir);
         $dotenv->load();
-    }
-
-    public function dispatchFastRoute()
-    {
-        require_once ROUTES_PATH . 'routes.php';
-        // Fetch method and URI from somewhere
-        $httpMethod = $_SERVER['REQUEST_METHOD'];
-        $uri = $_SERVER['REQUEST_URI'];
-
-        // Strip query string (?foo=bar) and decode URI
-        if (false !== $pos = strpos($uri, '?')) {
-            $uri = substr($uri, 0, $pos);
-        }
-        $uri = rawurldecode($uri);
-
-        $routeInfo = $dispatcher->dispatch($httpMethod, $uri);
-
-        switch ($routeInfo[0]) {
-            case \FastRoute\Dispatcher::NOT_FOUND:
-                return abort(404);
-                break;
-            case \FastRoute\Dispatcher::METHOD_NOT_ALLOWED:
-                $allowedMethods = $routeInfo[1];
-                return abort(405);
-                break;
-            case \FastRoute\Dispatcher::FOUND:
-                $handler = $routeInfo[1];
-                $vars = $routeInfo[2];
-                return $routeInfo[1]($httpMethod);
-                break;
-        }
     }
 }
