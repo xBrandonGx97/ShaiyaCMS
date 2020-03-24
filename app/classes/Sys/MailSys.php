0574c2c22043336469fed4c29736ab27291a674c
lunethdev <lunethdev@gmail.com>
2020-03-23 19:36:52 -0500

    update Message System class

--- app/classes/Sys/MailSys.php
+++ app/classes/Sys/MailSys.php
@@ -6,96 +6,98 @@
 
    class MailSys
    {
-       private static $mail;
-       private static $msgSubject;
-       private static $msgContent;
+       protected $mail;
+       private $msgSubject;
+       private $msgContent;
 
-       public static function sendMail($host, $mail_for, $data)
+       public function __construct($host)
        {
-           if ($host === 'gmail') {
-               self::messages($mail_for, $data);
-               self::mail_gmail();
-           } elseif ($host === 'localhost') {
-               self::mail_local();
+           $this->mail = new PHPMailer();
+           if ($host === 'local') {
+               $this->mail_local();
+           } elseif ($host === 'gmail') {
+               $this->mail_gmail();
            }
        }
 
-       public static function messages($mail_for, $data)
+       public function mail_local()
+       {
+           self::$mail = new PHPMailer();
+
+           self::$mail->isSMTP();
+           self::$mail->Host = config['mail']['host'];
+           self::$mail->Port = config['mail']['port'];
+
+           self::$mail->isHTML(true);
+           self::$mail->setFrom(config['mail']['reply_email'], config['mail']['reply_name']);
+           self::$mail->addAddress('userNametoSendTo@gmail.com');
+           self::$mail->Subject = self::$msgSubject;
+           self::$mail->Body = self::$msgContent;
+
+           if (self::$mail->send()) {
+               echo 'email sent';
+           } else {
+               echo 'error: ' . self::$mail->ErrorInfo;
+           }
+       }
+
+       public function mail_gmail()
+       {
+           $this->mail->isSMTP();
+           $this->mail->Host = config['mail']['host'];
+           $this->mail->SMTPAuth = config['mail']['auth'];
+           $this->mail->Username = config['mail']['user'];
+           $this->mail->Password = config['mail']['pass'];
+           $this->mail->Port = config['mail']['port'];
+           $this->mail->SMTPSecure = config['mail']['protocol'];
+
+           $this->mail->isHTML(true);
+           $this->mail->setFrom(config['mail']['reply_email'], config['mail']['reply_name']);
+           $this->mail->addAddress('brandonjm033@gmail.com');
+       }
+
+       public function sendMail($mail_for, $data)
+       {
+           $this->messages($mail_for, $data);
+           $this->mail->Subject = $this->msgSubject;
+           $this->mail->Body = $this->msgContent;
+           if ($this->mail->send()) {
+               echo 'email sent';
+           } else {
+               echo 'error: ' . $this->mail->ErrorInfo;
+           }
+       }
+
+       public function messages($mail_for, $data)
        {
            if ($mail_for === 'testEmail') {
-               self::$msgSubject = 'Hello - from the other side!';
-               self::$msgContent = 'Hi dood!</br>';
-               self::$msgContent .= 'Your username is: ' . $data[0];
+               $this->msgSubject = 'Hello - from the other side!';
+               $this->msgContent = 'Hi dood!</br>';
+               $this->msgContent .= 'Your username is: ' . $data;
            }
        }
 
-       public static function mail_local()
-       {
-           self::$mail = new PHPMailer();
-
-           self::$mail->isSMTP();
-           self::$mail->Host = config['mail']['host'];
-           self::$mail->Port = config['mail']['port'];
-
-           self::$mail->isHTML(true);
-           self::$mail->setFrom(config['mail']['reply_email'], config['mail']['reply_name']);
-           self::$mail->addAddress('userNametoSendTo@gmail.com');
-           self::$mail->Subject = self::$msgSubject;
-           self::$mail->Body = self::$msgContent;
-
-           if (self::$mail->send()) {
-               echo 'email sent';
-           } else {
-               echo 'error: ' . self::$mail->ErrorInfo;
-           }
-       }
-
-       public static function mail_gmail()
-       {
-           self::$mail = new PHPMailer();
-
-           self::$mail->isSMTP();
-           self::$mail->Host = config['mail']['host'];
-           self::$mail->SMTPAuth = config['mail']['auth'];
-           self::$mail->Username = config['mail']['user'];
-           self::$mail->Password = config['mail']['pass'];
-           self::$mail->Port = config['mail']['port'];
-           self::$mail->SMTPSecure = config['mail']['protocol'];
-
-           self::$mail->isHTML(true);
-           self::$mail->setFrom(config['mail']['reply_email'], config['mail']['reply_name']);
-           self::$mail->addAddress('userNametoSendTo@gmail.com');
-           self::$mail->Subject = self::$msgSubject;
-           self::$mail->Body = self::$msgContent;
-
-           if (self::$mail->send()) {
-               echo 'email sent';
-           } else {
-               echo 'error: ' . self::$mail->ErrorInfo;
-           }
-       }
-
-       public static function setMailHost()
+       public function setMailHost()
        {
        }
 
-       public static function setMailPort()
+       public function setMailPort()
        {
        }
 
-       public static function setMailFrom()
+       public function setMailFrom()
        {
        }
 
-       public static function addMailAddress()
+       public function addMailAddress()
        {
        }
 
-       public static function setMessageTitle()
+       public function setMessageTitle()
        {
        }
 
-       public static function setMessageContent()
+       public function setMessageContent()
        {
        }
    }
