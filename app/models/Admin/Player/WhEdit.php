e6bcf43e59aa584d49167e822ba21e10681bd66f
lunethdev <lunethdev@gmail.com>
2020-04-08 07:11:22 -0500

    add warehouse item edit

--- app/models/Admin/Player/WhEdit.php
+++ app/models/Admin/Player/WhEdit.php
@@ -11,34 +11,188 @@ class WhEdit
     public function __construct()
     {
         $this->data = new Utils\Data;
-        $this->guildName = isset($_POST['guild']) ? $this->data->purify(trim($_POST['guild'])) : false;
+        $this->charId = isset($_POST['CharID']) ? $this->data->purify(trim($_POST['CharID'])) : false;
+        $this->charName = isset($_POST['charName']) ? $this->data->purify(trim($_POST['charName'])) : false;
+        $this->userUid = isset($_POST['UserUID']) ? $this->data->purify(trim($_POST['UserUID'])) : false;
+        $this->userId = isset($_POST['userId']) ? $this->data->purify(trim($_POST['userId'])) : false;
+        $this->itemUid = isset($_POST['ItemUID']) ? $this->data->purify(trim($_POST['ItemUID'])) : false;
 
         $this->logSys = new LogSys;
     }
 
-    public function getGuildData()
+    public function getChar()
     {
-        $guild = DB::table(table('shCharData') . ' as c')
-            ->select('g.MasterName', 'g.GuildID', 'c.CharName', 'c.UserUID', 'c.UserID', 'c.CharID')
-            ->join(table('shGuildChars') . ' as  gc', 'c.CharID', '=', 'gc.CharID')
-            ->join(table('shGuilds') . ' as  g', 'gc.GuildID', '=', 'g.GuildID')
-            ->where('gc.GuildLevel', 2)
-            ->where('g.GuildName', $this->guildName)
+        $char = DB::table(table('shCharData'))
+            ->select()
+            ->where('CharName', $this->charName)
+            ->where('Del', 0)
             ->get();
-        return $guild;
+        return $char;
     }
 
-    public function removeGuildLeader()
+    public function getItems()
+    {
+        $items = DB::table(table('shUserWh') . ' as ci')
+            ->select('i.ItemName', 'ci.Slot', 'ci.Count', 'ci.ItemUID', 'ci.UserUID')
+            ->join(table('shItems') . ' as  i', 'i.ItemID', '=', 'ci.ItemID')
+            ->where('ci.UserUID', $this->userUid)
+            ->orderBy('ci.Slot')
+            ->get();
+        return $items;
+    }
+
+    public function getItemInfo()
+    {
+        $items = DB::select(DB::raw('SELECT c.Count,c.ItemID,c.Type,c.TypeID,c.Gem1,c.Gem2,c.Gem3,c.Gem4,c.Gem5,c.Gem6,
+        SUBSTRING(c.Craftname, 1, 2) AS Str, SUBSTRING(c.Craftname, 3, 2) AS Dex, SUBSTRING(c.Craftname, 5, 2) AS Rec,
+        SUBSTRING(c.Craftname, 7, 2) AS Int, SUBSTRING(c.Craftname, 9, 2) AS Wis, SUBSTRING(c.Craftname, 11, 2) AS Luc, SUBSTRING(c.Craftname, 13, 2) AS HP, SUBSTRING(c.Craftname, 15, 2) AS MP, SUBSTRING(c.Craftname, 17, 2) AS SP,
+        SUBSTRING(c.Craftname, 19, 2) AS Enchant, c.ItemUID, i.ItemName, i.ReqWis, i.Type
+        FROM ' . table('shUserWh') . ' c INNER JOIN ' . table('shItems') . ' i on i.ItemID = c.ItemID
+        WHERE c.ItemUID = ?'), [$this->itemUid]);
+        return $items;
+    }
+
+    public function updateItem()
     {
         try {
-            $update = DB::table(table('shGuildChars'))
-            ->where('GuildLevel', 1)
-            ->where('GuildID', $this->guildId)
-            ->update(['GuildLevel' => 8]);
-            $this->logSys->createLog('Removed guild leader of guild: ' . $this->guildName . ' old leader: ' . $this->oldGuildLeader);
+            $update = DB::table(table('shUserWh'))
+            ->where('ItemUID', $this->itemUid)
+            ->update(['ItemID' => $this->getItemId(), 'Type' => $this->getType(), 'TypeID' => $this->getTypeId(), 'Gem1' => $this->getGem1(), 'Gem2' => $this->getGem2(), 'Gem3' => $this->getGem3(), 'Gem4' => $this->getGem4(), 'Gem5' => $this->getGem5(), 'Gem6' => $this->getGem6(), 'Count' => $this->getCount(), 'Craftname' => $this->getCraftname()]);
+            $this->logSys->createLog('Edited item: ' . $this->getItemName() . ' of user: ' . $this->userId);
+            return 'Edited item: ' . $this->getItemName();
         } catch (\Illuminate\Database\QueryException $e) {
-            $this->logSys->createLog('Failed to removed guild leader of guild: ' . $this->guildName . ' old leader: ' . $this->oldGuildLeader);
-            return 'Could not remove guild leader.';
+            $this->logSys->createLog('Failed to edit item: ' . $this->getItemName() . ' of user: ' . $this->userId);
+            return 'Failed to edit item.';
         }
+    }
+
+    public function getGearTypes()
+    {
+        return [16, 17, 18, 19, 20, 21, 31, 32, 33, 34, 35, 36, 67, 68, 69, 70, 71, 72, 73, 74, 76, 77, 82, 83, 84, 85, 86, 87, 88, 89, 91, 92];
+    }
+
+    public function getColumns()
+    {
+        return ['ItemName', 'ItemUID', 'ItemID', 'Type', 'TypeID', 'Gem1', 'Gem2', 'Gem3', 'Gem4', 'Gem5', 'Gem6', 'Str', 'Dex', 'Rec', 'Int', 'Wis', 'Luc', 'HP', 'MP', 'SP', 'Enchant', 'Count'];
+    }
+
+    public function getGreyedColumns()
+    {
+        return ['ItemName', 'ItemUID'];
+    }
+
+    private function getItemId()
+    {
+        return isset($_POST['ItemID']) ? $this->data->purify(trim($_POST['ItemID'])) : false;
+    }
+
+    private function getItemName()
+    {
+        return isset($_POST['ItemName']) ? $this->data->purify(trim($_POST['ItemName'])) : false;
+    }
+
+    private function getType()
+    {
+        return isset($_POST['Type']) ? $this->data->purify(trim($_POST['Type'])) : false;
+    }
+
+    private function getTypeId()
+    {
+        return isset($_POST['TypeID']) ? $this->data->purify(trim($_POST['TypeID'])) : false;
+    }
+
+    private function getGem1()
+    {
+        return isset($_POST['Gem1']) ? $this->data->purify(trim($_POST['Gem1'])) : false;
+    }
+
+    private function getGem2()
+    {
+        return isset($_POST['Gem2']) ? $this->data->purify(trim($_POST['Gem2'])) : false;
+    }
+
+    private function getGem3()
+    {
+        return isset($_POST['Gem3']) ? $this->data->purify(trim($_POST['Gem3'])) : false;
+    }
+
+    private function getGem4()
+    {
+        return isset($_POST['Gem4']) ? $this->data->purify(trim($_POST['Gem4'])) : false;
+    }
+
+    private function getGem5()
+    {
+        return isset($_POST['Gem5']) ? $this->data->purify(trim($_POST['Gem5'])) : false;
+    }
+
+    private function getGem6()
+    {
+        return isset($_POST['Gem6']) ? $this->data->purify(trim($_POST['Gem6'])) : false;
+    }
+
+    private function getCount()
+    {
+        return isset($_POST['Count']) ? $this->data->purify(trim($_POST['Count'])) : false;
+    }
+
+    private function getStr()
+    {
+        return isset($_POST['Str']) ? $this->data->purify(trim($_POST['Str'])) : false;
+    }
+
+    private function getDex()
+    {
+        return isset($_POST['Dex']) ? $this->data->purify(trim($_POST['Dex'])) : false;
+    }
+
+    private function getRec()
+    {
+        return isset($_POST['Rec']) ? $this->data->purify(trim($_POST['Rec'])) : false;
+    }
+
+    private function getInt()
+    {
+        return isset($_POST['Int']) ? $this->data->purify(trim($_POST['Int'])) : false;
+    }
+
+    private function getWis()
+    {
+        return isset($_POST['Wis']) ? $this->data->purify(trim($_POST['Wis'])) : false;
+    }
+
+    private function getLuc()
+    {
+        return isset($_POST['Luc']) ? $this->data->purify(trim($_POST['Luc'])) : false;
+    }
+
+    private function getHp()
+    {
+        return isset($_POST['HP']) ? $this->data->purify(trim($_POST['HP'])) : false;
+    }
+
+    private function getMp()
+    {
+        return isset($_POST['MP']) ? $this->data->purify(trim($_POST['MP'])) : false;
+    }
+
+    private function getSp()
+    {
+        return isset($_POST['SP']) ? $this->data->purify(trim($_POST['SP'])) : false;
+    }
+
+    private function getEnchant()
+    {
+        return isset($_POST['Enchant']) ? $this->data->purify(trim($_POST['Enchant'])) : false;
+    }
+
+    private function getCraftname()
+    {
+        return $this->getStr() . $this->getDex() . $this->getRec() . $this->getInt() . $this->getWis() . $this->getLuc() . $this->getHp() . $this->getMp() . $this->getSp() . $this->getEnchant();
+    }
+
+    public function getNewValue($value)
+    {
+        return isset($_POST[$value]) ? $this->data->purify(trim($_POST[$value])) : false;
     }
 }
--- resources/views/pages/ap/player/editWhItems.blade.php
+++ resources/views/pages/ap/player/editWhItems.blade.php
@@ -1,6 +1,6 @@
 @extends('layouts.ap.app')
-@section('index', 'editWhItems')
-@section('title', 'Edit WhItems')
+@section('index', 'itemEdit')
+@section('title', 'Item Edit')
 @section('zone', 'AP')
 @section('content')
   @include('partials.ap.nav')
@@ -20,14 +20,158 @@
                     <div class="col-sm-12">
                       <div class="card align-items-center">
                         <div class="card-header">
-                          <h5>Delete Player Items</h5>
+                          <h5>Warehouse Item Edit</h5>
                         </div>
                         <div class="card-body">
                           @if (isset($_POST['submit']))
+                            @if (!empty($data['item']->charName))
+                              @if (count($data['item']->getChar()) > 0)
+                                <form method="post">
+                                  <div class="table-responsive">
+                                    <table class="table table-dark">
+                                      <thead>
+                                        <tr>
+                                          <th>CharName</th>
+                                          <th>UserID</th>
+                                          <th>UserUID</th>
+                                          <th>Select</th>
+                                        </tr>
+                                      </thead>
+                                      <tbody>
+                                        @foreach ($data['item']->getChar() as $res)
+                                          <tr>
+                                            <td>{{$res->CharName}}</td>
+                                            <td>{{$res->UserID}}</td>
+                                            <td>{{$res->UserUID}}</td>
+                                            <td><input type="radio" name="UserUID" value="{{$res->UserUID}}"></td>
+                                          </tr>
+                                        @endforeach
+                                      </tbody>
+                                    </table>
+                                  </div>
+                                  <p class="text-center">
+                                    <button type="submit" class="btn btn-sm btn-primary" name="submit2">Submit</button>
+                                  </p>
+                                  <input type="hidden" name="userId" value="{{$res->UserID}}"/>
+                                </form>
+                              @else
+                                Character doesn't exist. Please try again.
+                              @endif
+                            @else
+                              Character name can not be empty.
+                            @endif
+                          @elseif (isset($_POST['submit2']))
+                            @if (!empty($data['item']->userUid))
+                              @if (count($data['item']->getItems()) > 0)
+                                <form method="post">
+                                  <div class="table-responsive">
+                                    <table class="table table-striped">
+                                      <thead>
+                                        <tr>
+                                          <th>ItemName</th>
+                                          <th>Slot</th>
+                                          <th>Count</th>
+                                          <th>Select</th>
+                                        </tr>
+                                      </thead>
+                                      <tbody>
+                                        @foreach ($data['item']->getItems() as $res)
+                                          <tr>
+                                            <td>{{$res->ItemName}}</td>
+                                            <td>{{$res->Slot}}</td>
+                                            <td>{{$res->Count}}</td>
+                                            <td><input type="radio" name="ItemUID" value="{{$res->ItemUID}}"></td>
+                                          </tr>
+                                        @endforeach
+                                      </tbody>
+                                    </table>
+                                  </div>
+                                  <input type="hidden" name="userId" value="{{$data['item']->userId}}"/>
+                                  <input type="hidden" name="UserUID" value="{{$data['item']->userUid}}">
+                                  <p class="text-center">
+                                    <button type="submit" class="btn btn-sm btn-primary" name="submit3">Submit</button>
+                                  </p>
+                                </form>
+                              @else
+                                Could not find any items matching your provided account information.
+                              @endif
+                            @else
+                              You must select an account!
+                            @endif
+                          @elseif (isset($_POST['submit3']))
+                            @if (count($data['item']->getItemInfo()) > 0)
+                              <form method="post">
+                                <div class="table-responsive">
+                                  <table class="table table-striped">
+                                    @foreach ($data['item']->getItemInfo() as $res)
+                                      @foreach ($data['item']->getColumns() as $value)
+                                        <tr>
+                                          <th>{{$value}}</th>
+                                          @if (in_array($value, $data['item']->getGreyedColumns()))
+                                            <th>
+                                              <input type="text" class="form-control" name="{{$value}}" value="{{$res->$value}}" readonly/>
+                                            </th>
+                                          @else
+                                            @if ($value == 'Enchant')
+                                              <td>
+                                                <select class="form-control" name="{{$value}}">
+                                                  <option value="00">00</option>
+                                                  @if (in_array($res->Type, $data['item']->getGearTypes()))
+                                                    @for ($e = 50; $e <= 70; $e++)
+                                                      @if ($e == $res->$value)
+                                                        <option value="{{$e}}" selected>{{$e}}</option>
+                                                      @else
+                                                        <option value="{{$e}}">{{$e}}</option>
+                                                      @endif
+                                                    @endfor
+                                                  @else
+                                                    @for($a=1;$a <= 20; $a++)
+                                                      @if ($a == $res->$value)
+                                                        <option value="{{$a}}" selected>{{$a}}</option>
+                                                      @else
+                                                        <option value="{{$a}}">{{$a}}</option>
+                                                      @endif
+                                                    @endfor
+                                                  @endif
+                                                </select>
+                                              </td>
+                                            @else
+                                              <td>
+                                                <input type="text" class="form-control" name="{{$value}}" value="{{$res->$value}}"/>
+                                              </td>
+                                            @endif
+                                          @endif
+                                        </tr>
+                                      @endforeach
+                                    @endforeach
+                                  </table>
+                                </div>
+                                <input type="hidden" name="userId" value="{{$data['item']->userId}}"/>
+                                <input type="hidden" name="UserUID" value="{{$data['item']->userUid}}">
+                                <p class="text-center">
+                                  <button type="submit" class="btn btn-sm btn-primary" name="submit4">Submit</button>
+                                </p>
+                              </form>
+                            @else
+                              Failed to fetch item data.
+                            @endif
+                          @elseif (isset($_POST['submit4']))
+                            {{$data['item']->updateItem()}}
+                            <br>
+                            @foreach ($data['item']->getItemInfo() as $res)
+                              @foreach ($data['item']->getColumns() as $value)
+                                @if (!in_array($value, $data['item']->getGreyedColumns()))
+                                  {{$value}} => {{$data['item']->getNewValue($value)}}<br>
+                                @endif
+                              @endforeach
+                            @endforeach
+                            <p class="text-center">
+                              <button type="button" onclick="window.location.href='{{$_SERVER['REQUEST_URI']}}'" class="btn btn-sm btn-primary" name="return">Return back to warehouse item edit</button>
+                            </p>
                           @else
                             <form method="post">
                               <div class="form-group mx-sm-3 mb-2">
-                                <input type="text" class="form-control" placeholder="Character name" name="userId">
+                                <input type="text" class="form-control" placeholder="Character name" name="charName">
                               </div>
                               <p class="text-center">
                                 <button type="submit" class="btn btn-sm btn-primary" name="submit">Submit</button>
